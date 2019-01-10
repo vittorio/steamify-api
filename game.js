8aa7411d41664c15ff0a75f4916d70842e804e46
Vittorio <sexsportandrocknroll@gmail.com>
2019-01-10 14:39:03 +0300

    firebase version

--- game.js
+++ game.js
@@ -1,68 +1,19 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const GameSchema = new Schema({
-  basic: {
-    appid: Number,
-    name: String,
-    playtimeTwoWeeks: Number,
-    playtimeTwoWeeksReadable: String,
-    playtimeForever: Number,
-    playtimeForeverReadable: String,
-    icon: String,
-    logo: String,
-    header: String,
-    hasCommunityVisibleStats: Boolean
+game = {
+  "header": "https://steamcdn-a.akamaihd.net/steam/apps/10500/header.jpg?t=1545151872",
+  "price": {
+    "currency": "RUB",
+    "initial": 102700,
+    "final": 102700,
+    "discount_percent": 0,
+    "initial_formatted": "",
+    "final_formatted": "1027 pуб."
   },
-
-  details: {
-    id: Number,
-    name: String,
-    controllerSupport: String,
-    description: String,
-    about: String,
-    header: String,
-    website: String,
-    pcRequirements: {
-      minimum: String
-    },
-    legal: String,
-    developers: [String],
-    publishers: [String],
-    price: {
-      currency: String,
-      initial: Number,
-      final: Number,
-      discount_percent: Number,
-      initial_formatted: String,
-      final_formatted: String
-    },
-    platforms: {
-      windows: Boolean,
-      mac: Boolean,
-      linux: Boolean
-    },
-    metacritic: {
-      score: Number,
-      url: String
-    },
-    categories: [{
-      id: Number,
-      description: String
-    }],
-    genres: [{
-      id: Number,
-      description: String
-    }],
-    release: {
-      coming_soon: Boolean,
-      date: String
-    }
-  },
-
-  extras: {
-    actual_price: Number
-  }
-});
-
-module.exports = mongoose.model('Games', GameSchema);
+  "platforms": {"windows": true, "mac": true, "linux": true},
+  "metacritic": {"score": "No Score"},
+  "categories": [{"id": 2, "description": "Single-player"}, {"id": 1, "description": "Multi-player"}, {
+    "id": 22,
+    "description": "Steam Achievements"
+  }, {"id": 29, "description": "Steam Trading Cards"}, {"id": 15, "description": "Stats"}],
+  "genres": [{"id": "2", "description": "Strategy"}],
+  "release": {"coming_soon": false, "date": "4 Mar, 2009"}
+}
--- server.js
+++ server.js
@@ -1,18 +1,37 @@
 const express = require('express');
 const cors = require('cors');
 const SteamApi = require('steam-api');
-const mongoose = require('mongoose');
+const _ = require('lodash');
 
-const Game = require('./game');
-
-const apiKey = '2D7A611545098E1FC165D53E16F03065';
+const apiKey = '1AD897533C698E617B4F351C640EC53E';
 const userSteamId = '76561198080321262';
 
 let steamApp = new SteamApi.App(apiKey);
 let player = new SteamApi.Player(apiKey, userSteamId);
 
 const app = express();
-mongoose.connect('mongodb://localhost:27017/steamify', {useNewUrlParser: true});
+
+const firebase = require('firebase');
+const admin = require("firebase-admin");
+const firebaseConfig = {
+  apiKey: "AIzaSyD5KmSQcQ1CLDeyfEdnIl7SDCrTwc0GXFQ",
+  authDomain: "steamify-61hub.firebaseapp.com",
+  databaseURL: "https://steamify-61hub.firebaseio.com",
+  projectId: "steamify-61hub",
+  storageBucket: "steamify-61hub.appspot.com",
+  messagingSenderId: "497608429923"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const serviceAccount = require('./steamify-61hub-firebase-adminsdk-l80ee-0150531b1d.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://steamify-61hub.firebaseio.com"
+});
+
+const db = admin.firestore();
 
 app.use(cors());
 
@@ -22,63 +41,113 @@ const onError = (res) => {
   });
 };
 
+const getAllGamesFromFirebase = async () => {
+  const snapchot = await db.collection('games').get();
+
+  const docs = [];
+
+  snapchot.forEach(doc => docs.push(doc.data()));
+
+  return docs
+};
+
+const getGameMainInformation = ({
+                                  appId,
+                                  name = '',
+                                  playtimeTwoWeeks = 0,
+                                  playtimeTwoWeeksReadable = '',
+                                  playtimeForever = 0,
+                                  playtimeForeverReadable = '',
+                                  icon = '',
+                                  logo = '',
+                                  header = '',
+                                  lastSync = Date.now()
+                                }) => ({
+  appId,
+  name,
+  playtimeTwoWeeks,
+  playtimeTwoWeeksReadable,
+  playtimeForever,
+  playtimeForeverReadable,
+  icon,
+  logo,
+  header,
+  lastSync
+})
+
+
+let storedGames;
+let once = false
+
 app.get('/api/v1/games', async (req, res) => {
   let games = await player.GetOwnedGames(
     userSteamId,
     optionalIncludeAppInfo = true,
     optionalIncludePlayedFreeGames = true,
     optionalAppIdsFilter = []
-  )
-    .catch(() => onError(res));
+  ).catch(() => onError(res));
 
   if (games && games.length) {
-    const games2 = [];
-    await Promise.all(
-      games.map(game => {
-        return new Promise(async resolve => {
-          let result = await Game.findOneAndUpdate({'basic.appid': game.appid}, {$set: {basic: game}}, {upsert: true, new: true});
-          if (!result.details) {
-            let result2 = await fetchAppDetails(game.appid)
-
-            if (result2) {
-              result = result2;
-            }
-          }
-
-          games2.push(result);
-          resolve(result)
-        })
-      })
-    );
-
-    games = games2;
-  }
-  else {
-    games = await Game.find({});
-  }
-
-  res.json(games);
-});
-
-const fetchAppDetails = (appId) => {
-  return new Promise(async (resolve, reject) => {
-    const game = await steamApp.appDetails(appId)
-      .catch(reject);
-
-    let result;
-
-    if (game && game.id) {
-      result = await Game.findOneAndUpdate({'basic.appid': appId}, {$set: {details: game}}, {new: true});
-    } else {
-      result = await Game.findOne({'basic.appid': appId});
+    if (!storedGames) {
+      storedGames = await getAllGamesFromFirebase();
     }
 
-    resolve(result);
-  })
-};
+    await Promise.all(
+      games.map(async game => {
+        let { appId } = game;
+        let savedGame = getGameMainInformation(game);
+        appId = appId.toString();
+
+        if (appId.length) {
+          const hasAlreadyGame = _.some(storedGames, { appId });
+          const isPlaytimeChanged = hasAlreadyGame & _.some(storedGames, g => (g.appId === appId) && g.playtimeForever !== savedGame.playtimeForever);
+
+          // const storedGame = _.find(storedGames, {appId});
+          // const hasDetails = hasAlreadyGame && (storedGame.price || storedGame.metacritic || storedGame.categories)
+          //
+          // if (!hasDetails && !once) {
+          //   if (storedGame && storedGame.hasNoDetails) {
+          //
+          //   } else {
+          //
+          //     once = true
+          //     const details = await steamApp.appDetails(appId)
+          //       .catch((e) => {
+          //         db.collection('errors').doc(Date.now() + '').set({
+          //           appId: appId,
+          //           details: true,
+          //           errorCode: e.response.statusCode
+          //         });
+          //       });
+          //
+          //     console.log(appId, details);
+          //
+          //     if (details) {
+          //       const { price = {}, platforms = [], metacritic = '', categories = [], genres = [], release = {}} = details
+          //
+          //       savedGame = { ...savedGame,  price, platforms, metacritic, categories, genres, release }
+          //     }
+          //   }
+          // }
+
+          if (storedGames && (!hasAlreadyGame || isPlaytimeChanged)) {
+            const index = _.findIndex(storedGames, { appId });
+            storedGames.splice(index, 1, savedGame);
+
+            return db.collection('games').doc(appId).set(savedGame);
+          }
+        } else {
+          return db.collection('errors').doc(Date.now() + '').set(savedGame);
+        }
+      })
+    )
+  }
+
+  res.json(storedGames);
+});
 
 app.get('/api/v1/games/:id', async (req, res) => {
-  const result = await fetchAppDetails(req.params.id)
+  const result = await steamApp.appDetails(req.params.id)
     .catch(() => onError(res));
 
   res.json(result);
