4b8620c7e991587f8101839d0c5f509512cb8fc8
Vittorio <sexsportandrocknroll@gmail.com>
2019-01-20 19:11:57 +0300

    back to mongo

--- game.js
+++ game.js
@@ -1,19 +1,18 @@
-game = {
-  "header": "https://steamcdn-a.akamaihd.net/steam/apps/10500/header.jpg?t=1545151872",
-  "price": {
-    "currency": "RUB",
-    "initial": 102700,
-    "final": 102700,
-    "discount_percent": 0,
-    "initial_formatted": "",
-    "final_formatted": "1027 pуб."
-  },
-  "platforms": {"windows": true, "mac": true, "linux": true},
-  "metacritic": {"score": "No Score"},
-  "categories": [{"id": 2, "description": "Single-player"}, {"id": 1, "description": "Multi-player"}, {
-    "id": 22,
-    "description": "Steam Achievements"
-  }, {"id": 29, "description": "Steam Trading Cards"}, {"id": 15, "description": "Stats"}],
-  "genres": [{"id": "2", "description": "Strategy"}],
-  "release": {"coming_soon": false, "date": "4 Mar, 2009"}
-}
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const GameSchema = new Schema({
+  playtimeTwoWeeksReadable: String,
+  name: String,
+  playtimeForeverReadable: String,
+  appId: Number,
+  playtimeTwoWeeks: Number,
+  header: String,
+  icon: String,
+  price: {type: Number, default: 0},
+  playtimeForever: Number,
+  logo: String,
+  lastSync: Number
+});
+
+module.exports = mongoose.model('Games', GameSchema);
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+const mongoose = require("mongoose");
 const express = require('express');
 const cors = require('cors');
 const SteamApi = require('steam-api');
 const _ = require('lodash');
 const bodyParser = require('body-parser');
-const admin = require("firebase-admin");
-const serviceAccount = require('./steamify-61hub-firebase-adminsdk-l80ee-0150531b1d.json');
+
+const Game = require('./game');
 
 const apiKey = '1AD897533C698E617B4F351C640EC53E';
 const userSteamId = '76561198080321262';
@@ -12,17 +13,10 @@ const userSteamId = '76561198080321262';
 let steamApp = new SteamApi.App(apiKey);
 let player = new SteamApi.Player(apiKey, userSteamId);
 
+mongoose.connect('mongodb://localhost:27017/steamify', {useNewUrlParser: true});
+
 const app = express();
-
-app.use(bodyParser.json({ type: 'application/json' }));
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://steamify-61hub.firebaseio.com"
-});
-
-const db = admin.firestore();
-
+app.use(bodyParser.json({type: 'application/json'}));
 app.use(cors());
 
 const onError = (res) => {
@@ -31,42 +25,7 @@ const onError = (res) => {
   });
 };
 
-const getAllGamesFromFirebase = async () => {
-  const snapchot = await db.collection('games').get();
-
-  const docs = [];
-
-  snapchot.forEach(doc => docs.push(doc.data()));
-
-  return docs
-};
-
-const getGameMainInformation = ({
-                                  appId,
-                                  name = '',
-                                  playtimeTwoWeeks = 0,
-                                  playtimeTwoWeeksReadable = '',
-                                  playtimeForever = 0,
-                                  playtimeForeverReadable = '',
-                                  icon = '',
-                                  logo = '',
-                                  header = '',
-                                  lastSync = Date.now()
-                                }) => ({
-  appId,
-  name,
-  playtimeTwoWeeks,
-  playtimeTwoWeeksReadable,
-  playtimeForever,
-  playtimeForeverReadable,
-  icon,
-  logo,
-  header,
-  lastSync
-});
-
-
-let storedGames;
+const excludedField = '-_id -__v'
 
 app.get('/api/v1/games', async (req, res) => {
   let games = await player.GetOwnedGames(
@@ -76,39 +35,27 @@ app.get('/api/v1/games', async (req, res) => {
     optionalAppIdsFilter = []
   ).catch(() => onError(res));
 
-  if (games && games.length) {
-    if (!storedGames) {
-      storedGames = await getAllGamesFromFirebase();
-    }
+  games = Array.isArray(games) ? games : [];
 
-    await Promise.all(
-      games.map(async game => {
-        let { appId } = game;
-        let savedGame = getGameMainInformation(game);
-        appId = appId.toString();
-
-        if (appId.length) {
-          const hasAlreadyGame = _.some(storedGames, g => (g.appId == appId));
-          const isPlaytimeChanged = hasAlreadyGame & _.some(storedGames, g => (g.appId == appId) && g.playtimeForever !== savedGame.playtimeForever);
-
-          if (!hasAlreadyGame || isPlaytimeChanged) {
-            if (!hasAlreadyGame) {
-              return db.collection('games').doc(appId).set(savedGame);
-            } else {
-              const index = _.findIndex(storedGames, { appId });
-              storedGames.splice(index, 1, savedGame);
-
-              return db.collection('games').doc(appId).update(savedGame);
-            }
-          }
-        } else {
-          return db.collection('errors').doc(Date.now() + '').set(savedGame);
-        }
+  if (games.length) {
+    games = await Promise.all(
+      games.map(g => {
+        return Game.findOneAndUpdate(
+          {appId: g.appId},
+          g,
+          {
+            upsert: true,
+            new: true,
+            setDefaultsOnInsert: true,
+            fields: excludedField
+          })
       })
-    )
+    );
+  } else {
+    games = await Game.find({}, excludedField)
   }
 
-  res.json(storedGames);
+  res.json(games);
 });
 
 app.get('/api/v1/games/:id', async (req, res) => {
@@ -119,24 +66,25 @@ app.get('/api/v1/games/:id', async (req, res) => {
 });
 
 app.patch('/api/v1/games/:id', async (req, res) => {
-  let { price } = req.body;
+  let {price} = req.body;
 
-  if (price !== "" && isNaN(parseInt(price))) {
+  if (typeof price !== 'number') {
     res.statusCode = 400;
-    res.send('Wrong price format');
-    return
+    return res.send('The price must have an integer type');
   }
 
-  price = price || "";
+  const game = await Game.findOneAndUpdate(
+    {appId: req.params.id},
+    {price},
+    {
+      new: true,
+      fields: excludedField
+    });
 
-  await db.collection('games').doc(req.params.id).update({ price });
-  const index = _.findIndex(storedGames, g => g.appId == req.params.id );
-  storedGames[index].price = price;
-  res.statusCode = 200;
-  return res.send('Updated successfully');
+  return res.json(game);
 });
 
 
 app.listen(3000, () => {
-  console.log('Steam holder server');
+  console.log('Steamify api server');
 });
